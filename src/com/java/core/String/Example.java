18fe891d03aa9426211a38115fb501853f974378
DhajekarSuyash <suyash.dhajekar18@gmail.com>
2026-05-05 19:06:53 +0530

    String session one

---
+++ src/com/java/core/String/Example.java
@@ -0,0 +1,21 @@
+package com.java.core.String;
+
+public class Example {
+	
+	public static void main(String[] args) {
+		
+		String s = "TUESDAY";
+		
+		char c = s.charAt(2);
+		System.out.println(c);
+		
+		for(int i = 0 ; i <= s.length(); i++ ) {
+			
+			if(i == 2) {
+				System.out.println(s.charAt(i));
+			}
+			
+		}
+	}
+
+}
