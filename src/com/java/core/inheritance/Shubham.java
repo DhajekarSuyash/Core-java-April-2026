fa8fb0a8bd8735869e84af4f3a9b8e64ef58554a
DhajekarSuyash <suyash.dhajekar18@gmail.com>
2026-05-19 18:53:27 +0530

    inheritance - variable

---
+++ src/com/java/core/inheritance/Shubham.java
@@ -0,0 +1,15 @@
+package com.java.core.inheritance;
+
+//CHILD
+public class Shubham  extends Student
+{
+	public Shubham() {
+		
+	}
+	
+  public int b = 200;
+  
+  public int c = 400;
+  
+  public int age = 7;
+}
---
+++ src/com/java/core/inheritance/Student.java
@@ -0,0 +1,15 @@
+package com.java.core.inheritance;
+
+//PARENT
+public class Student {
+	
+	public Student() {
+		
+	}
+	
+	public int a =	12;
+
+	public int d = 50;
+	
+	public int age = 25;
+}
