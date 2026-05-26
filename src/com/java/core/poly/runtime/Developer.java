55fa24c7f28c3af6375cbd4a48fe9cfda9c4f912
Suyash <suyash.dhajekar18@gmail.com>
2026-05-26 18:56:51 +0530

    abstraction

--- src/com/java/core/poly/runtime/Developer.java
+++ src/com/java/core/poly/runtime/Developer.java
@@ -1,12 +1,8 @@
 package com.java.core.poly.runtime;
 
 public class Developer extends Employee {
-
 	@Override
 	public void printSalary() {
-		System.out.println("Developer : 4000" );
+		System.out.println("Developer : 4000");
 	}
-
-	
-	
 }
